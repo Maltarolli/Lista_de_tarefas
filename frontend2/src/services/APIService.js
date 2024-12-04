7ae5b69e830cc7d253a48ff1c722ae6fe4846f25
Pedro Roberto <maltarollipedro@gmail.com>
2024-12-04 00:45:26 -0300

    Pequenas alterações

--- frontend2/src/services/APIService.js
+++ frontend2/src/services/APIService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API_URL = process.env.REACT_APP_API_URL;
-const tarefaId = 12
+const tarefaId = 13
 const titulo = "mudar"
 const descricao = "mudar"
 const status = "Concluido"
