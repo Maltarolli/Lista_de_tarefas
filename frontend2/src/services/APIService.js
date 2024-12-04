40235f72b9c6a0ab75f458e594031fcc972a304c
Pedro Roberto <maltarollipedro@gmail.com>
2024-12-04 01:09:05 -0300

    Correção para usar em outros computadores

--- frontend2/src/services/APIService.js
+++ frontend2/src/services/APIService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API_URL = process.env.REACT_APP_API_URL;
-const tarefaId = 13
+const tarefaId = 14
 const titulo = "mudar"
 const descricao = "mudar"
 const status = "Concluido"
