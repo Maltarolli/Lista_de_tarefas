9fcbc85e73ccaf4c761bb74880fc222961777be4
Pedro Roberto <maltarollipedro@gmail.com>
2024-12-12 11:21:49 -0300

    Atualização do banco de dados

--- backend/database/create_db.py
+++ backend/database/create_db.py
@@ -3,10 +3,11 @@ import sqlite3
 # Nome do banco de dados
 DATABASE_NAME = "LISTA_DE_TAREFAS.db"
 
-def criar_banco_e_tabela():
+def criar_banco_e_tabelas():
     conn = sqlite3.connect(DATABASE_NAME)
     try:
         cursor = conn.cursor()
+        # Criação da tabela de tarefas
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS tarefas (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,10 +18,20 @@ def criar_banco_e_tabela():
                 update_tarefa TEXT NOT NULL
             )
         """)
+
+        # Criação da tabela de usuários
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                senha TEXT NOT NULL
+            )
+        """)
         conn.commit()
-        print("Banco e tabela criados com sucesso!")
+        print("Banco e tabelas criados com sucesso!")
     finally:
         conn.close()
 
 if __name__ == "__main__":
-    criar_banco_e_tabela()
+    criar_banco_e_tabelas()
